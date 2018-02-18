af61c6ab8921ccb07401a431f8037c08aec7d986
Danakt Frost <danakt_frost@mail.ru>
2018-02-18 23:44:49 +0300

    Add instruments store

---
+++ src/stores/instruments.ts
@@ -0,0 +1,52 @@
+/** @todo Remove the eslint stub below */
+// eslint-disable-next-line no-unused-vars
+import { observable, action } from 'mobx'
+import * as instruments from '../constants/instruments'
+
+type Instrument = typeof instruments
+
+export default class StoreInstruments {
+  /** Instruments */
+  @observable public instruments = instruments
+
+  /**
+   * Current preset
+   * Default: 'guitar'
+   */
+  @observable public currentInstrument: keyof Instrument = 'guitar'
+
+  /** Current tuning */
+  @observable public currentTuningIndex: number = 0
+
+  /**
+   * Changes the instument
+   * @param {string} newInstrumentName Instrument name
+   * @param {number} newTuningIndex Tuning index
+   */
+  @action
+  changeInstrument(
+    newInstrumentName: keyof Instrument,
+    newTuningIndex: number
+  ): void {
+    // If instrument name is not valid
+    const instrument = instruments[newInstrumentName]
+    if (!instrument) {
+      return
+    }
+
+    this.changeTuning(newTuningIndex)
+  }
+
+  /***
+   * Changes the tuning
+   * @param {number} newTuningIndex Index of the tuning
+   */
+  @action
+  changeTuning(newTuningIndex: number): void {
+    const instrument = instruments[this.currentInstrument]
+
+    // If tuning index is not valid
+    this.currentTuningIndex
+      = instrument.tunings[newTuningIndex] !== undefined ? newTuningIndex : 0
+  }
+}
