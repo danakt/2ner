cbb4ebed114ae73ea65385527ee88398dba00f2a
Danakt Frost <danakt_frost@mail.ru>
2018-02-25 02:30:08 +0300

    Add pitch updating to store

--- src/stores/media.ts
+++ src/stores/media.ts
@@ -1,6 +1,13 @@
 import { observable, action } from 'mobx'
+import { startAudioProcessing } from '~/libs/audio'
 
 export class StoreMedia {
+  /** Varialbe for saving function to cancel pitch update */
+  private _stopAudioProcessing: void | VoidFunction = undefined
+
+  /** Current audio pitch */
+  @observable public currentPitch = 0
+
   /** Audio stream */
   @observable public audioStream: MediaStream | null | void = undefined
 
@@ -9,8 +16,31 @@ export class StoreMedia {
    * @param {MediaStream} stream
    */
   @action
-  public setAudioStream(stream: MediaStream | void | null): void {
+  public async setAudioStream(stream: MediaStream | void | null) {
     this.audioStream = stream
+
+    // Stop previous audio processing
+    if (typeof this._stopAudioProcessing === 'function') {
+      this._stopAudioProcessing()
+    }
+
+    // Set new pitch updating
+    if (stream != null) {
+      this._stopAudioProcessing = await startAudioProcessing(
+        stream as MediaStream,
+        pitch => this.updatePitch(pitch)
+      )
+    }
+  }
+
+  /**
+   * Updates the pitch
+   * @param pitch The pitch
+   */
+  @action
+  public updatePitch(pitch: number) {
+    // this.currentPitch = Math.random() * 150 + 60
+    this.currentPitch = pitch
   }
 }
 
